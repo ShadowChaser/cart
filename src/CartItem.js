1f39eb5596be76051b1f949d2bc9ad19b423dd6a
Abhishek <abhishekkundu86@gmail.com>
2022-02-20 11:24:37 +0530

    added delete plus minus icons

--- src/CartItem.js
+++ src/CartItem.js
@@ -13,6 +13,9 @@ class CartItem extends React.Component{
                     <div style={{color: '#777'}}>Qty: 1</div>
                     <div className="cart-item-actions">
                         {/*Buttons */}
+                        <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/00/70/16/29/1000_F_70162903_5mFpUbO3ZfRyD4gslH8j2c5VxjGMKU9G.jpg"/>
+                        <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"/>
+                        <img alt="delete" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"/>
                     </div>
                 </div>
             </div>
