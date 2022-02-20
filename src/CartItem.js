4bf6912ae0073b28aa49c1f545e66928aa592e3e
Abhishek <abhishekkundu86@gmail.com>
2022-02-20 15:54:19 +0530

    added condition in decrease quntity if total is 0 it won't work

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,22 +11,65 @@ class CartItem extends React.Component{
             img:''
         }
     }
+
+    increaseQuantity =()=>{ // Arrow function will bind this with this function
+        // console.log("this.state",this.state);
+        //setState form 1
+        // this.setState({
+        //     qty: this.state.qty+1
+        // });
+        //setState form 2 - if prev State required use this
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty+1
+            }
+        });
+    }
+
+    decreaseQuantity =() =>{
+        const {qty} =this.state;
+
+        if(qty==0){
+            return;
+        }
+            
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty-1
+            }
+        });
+    }
     render (){
-        const {price,title,qty}=this.state; //getting property from js
+        const {price,title,qty}=this.state; //getting property from js by objectd de-structuring
+       
         return(
             <div className="cart-item">
                 <div className="left-block">
                    <img style={styles.image}/>
                 </div>
-                <div class="right-block">
+                <div className="right-block">
                     <div style={{fontSize:25}}>{title}</div>
                     <div style={{color: '#777'}}>Rs {price}</div>
                     <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/*Buttons */}
-                        <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/00/70/16/29/1000_F_70162903_5mFpUbO3ZfRyD4gslH8j2c5VxjGMKU9G.jpg"/>
-                        <img alt="decrease" className="action-icons" src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"/>
-                        <img alt="delete" className="action-icons" src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"/>
+                        <img 
+                        alt="increase" 
+                        className="action-icons" 
+                        src="https://as2.ftcdn.net/v2/jpg/00/70/16/29/1000_F_70162903_5mFpUbO3ZfRyD4gslH8j2c5VxjGMKU9G.jpg" 
+                        onClick={this.increaseQuantity}
+                        />
+                        <img 
+                        alt="decrease" 
+                        className="action-icons" 
+                        src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+                        onClick={this.decreaseQuantity}
+                        />
+                        <img 
+                        alt="delete" 
+                        className="action-icons" 
+                        src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"
+                        />
                     </div>
                 </div>
             </div>
